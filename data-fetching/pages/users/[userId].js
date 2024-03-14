4e38b749cde0c1c48c2591248d55f7c3b5a30dfc
RASAM KAMYAR <rasamkamyar@gmail.com>
2024-03-14 16:13:27 +0330

    add getStaticPaths

--- data-fetching/pages/users/[userId].js
+++ data-fetching/pages/users/[userId].js
@@ -1,5 +1,39 @@
-// function User(){
-//     return(
+function UserDetail({ data }) {
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <h2>{data.email}</h2>
+    </div>
+  );
+}
 
-//     )
-// }
+export default UserDetail;
+
+export async function getStaticPaths() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  const paths = data.map((user) => ({
+    params: {
+      userId: user.id.toString(),
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${params.userId}`
+  );
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
