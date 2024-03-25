64919811fd22a407f97eabe498133c50dd795a88
RASAM KAMYAR <rasamkamyar@gmail.com>
2024-03-25 17:32:40 +0330

    init

---
+++ api-router/data/todos.js
@@ -0,0 +1,18 @@
+export const todos = [
+  {
+    todo: 1,
+    "user": "A",
+  },
+  {
+    todo: 2,
+    "user": "B",
+  },
+  {
+    todo: 3,
+    "user": "C",
+  },
+  {
+    todo: 4,
+    "user": "D",
+  },
+];
